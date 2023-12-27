7ac78f8f13cf401a9c7fb1151a8f0da289d84af8
DanielTreep <danieltreep@live.nl>
2023-12-27 13:20:11 +0100

    data store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
   <main class="p-5">
-    <div class="container">
+    <div class="container" v-if="data">
       <input type="text" />
+      <div v-for="(client, index) in data.clients" :key="index">
+        {{ client.client_name }}
+        <ul>
+          <!-- <li v-for="(category, index) in data.project_categories" :key="index"></li> -->
+        </ul>
+      </div>
       <select name="" id="">
         <optgroup label="bedrijf">
-          <option value="">Project</option>
+          <option value="" v-for="(client, index) in data.clients" :key="index">
+            {{ client.client_name }}
+          </option>
         </optgroup>
       </select>
       <Timer />
@@ -14,4 +22,16 @@
 
 <script setup>
 import Timer from "@/components/Timer.vue";
+import { onMounted } from "vue";
+import { useDataStore } from "@/stores/data";
+import { storeToRefs } from "pinia";
+const { data } = storeToRefs(useDataStore());
+
+onMounted(() => {
+  fetch(
+    "https://my-json-server.typicode.com/danieltreep/merkelijkheid-timer/db"
+  )
+    .then((res) => res.json())
+    .then((db) => (data.value = db));
+});
 </script>
